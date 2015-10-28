5711c39c03bec660b7df1f121e1ca5501bceaa25
Felipe Coury <felipe.coury@gmail.com>
2015-10-28 12:44:47 -0200

    Improved cmd.sh to take all command line args

--- cmd.sh
+++ cmd.sh
@@ -1,1 +1,8 @@
-curl -X POST http://localhost:3030/slack_hook -d "text=\!$1&trigger_word=\!&user_name=fcoury"
+if [ -f .env ]; then
+  export $(cat .env | xargs)
+else
+  echo 'Missing .env file, see .env.example for reference'
+  exit 1
+fi
+
+curl -X POST ${APP_URL}slack_hook -d "text=\!$*&trigger_word=\!&user_name=fcoury&channel_name=other"
